d9fff677f55f0b0d7857e9c897a16687f6cb1087
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2014-06-08 17:19:33 +0200

    blockwise filters

---
+++ python/python_module/skneuro/blockwise_filters/__init__.py
@@ -0,0 +1,26 @@
+from ..utilities import blockwiseCaller
+from .. import denoising
+from multiprocessing import cpu_count
+import numpy
+
+
+def blockwiseGaussianSmoothing(image, sigma, out=None, blockShape=None, nThreads=None):
+    shape = image.shape
+
+    if blockShape is None:
+        blockShape = [min(s, 100) for s in shape]
+
+    if nThreads is None:
+        nThreads = cpu_count()
+
+    if out is None:
+        out = numpy.empty(shape=shape,dtype=numpy.float32)
+
+    margin = int(2.0*sigma +1.0+0.5)
+
+    blockwiseCaller(f=denoising.gaussianSmoothing,margin=margin,shape=shape,
+                    blockShape=blockShape, nThreads=nThreads,
+                    inputKwargs=dict(image=image),
+                    paramKwagrs=dict(sigma=sigma),
+                    out=out)
+    return out

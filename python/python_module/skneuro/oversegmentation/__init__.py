fee313f8535a76032730f9654b62e6d7bdb78e0b
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2014-07-15 16:54:49 +0200

    minor superpixel changes

--- python/python_module/skneuro/oversegmentation/__init__.py
+++ python/python_module/skneuro/oversegmentation/__init__.py
@@ -2,9 +2,10 @@ from _oversegmentation import *
 import vigra
 from vigra import numpy
 import skneuro.denoising as denoise
+from  skneuro import addHocViewer
 
 def pmapWatershed(pmap, raw, visu=True, seedThreshold=0.6):
-    import skneuro
+
     viewGrayData = [(pmap, "pmap") ]
     viewLabelData= []
 
@@ -19,7 +20,7 @@ def pmapWatershed(pmap, raw, visu=True, seedThreshold=0.6):
     viewGrayData.append((pmapD, "pmapTotalVariation"))
     viewGrayData.append((raw, "raw"))
 
-    #skneuro.addHocViewer(viewGrayData, viewLabelData, visu=visu)
+    #addHocViewer(viewGrayData, viewLabelData, visu=visu)
 
     print "compute local minima "
 
@@ -57,7 +58,7 @@ def pmapWatershed(pmap, raw, visu=True, seedThreshold=0.6):
 
 
     viewLabelData.append([seg, "seg"])
-    skneuro.addHocViewer(viewGrayData, viewLabelData, visu=visu)
+    addHocViewer(viewGrayData, viewLabelData, visu=visu)
 
     return se
 
@@ -75,62 +76,88 @@ def restrictedLocalMinima(image, minAllowed, neighborhood=26):
 
 
 
-def prepareMinMap(raw, pmap, sPre=0.8, sInt = 6.0, mapInterval = 0.3,
-                  scaleEw=4.0,ewBeta=0.01
-                  tvWeightSoft= None, isotropicTvSoft=True
-                  tvWeightHard= None, isotropicTvHard=True,
-                  sPost=0.6
+def prepareMinMap(raw, pmap, sPre=0.8, sInt=5.0, mapInterval=0.5,
+                  scaleEw=4.0, ewBeta=0.01,
+                  tvWeightSoft=None, isotropicTvSoft=True,
+                  tvWeightHard=None, isotropicTvHard=True,
+                  sPost=0.6, visu=False
                 ):
     """
 
     """
+
+
+
     if tvWeightSoft is None and isotropicTvSoft:
-        tvWeightSoft=4.0
+        tvWeightSoft=5.0
     elif tvWeightSoft is None and isotropicTvSoft==False:
         tvWeightSoft=25.0
 
-    if isotropicTvHard is None and isotropicTvHard:
-        isotropicTvHard=0.5
-    elif isotropicTvHard is None and isotropicTvHard==False:
-        isotropicTvHard=15.0
+    if tvWeightHard is None and isotropicTvHard:
+        tvWeightHard=0.7
+    elif tvWeightHard is None and isotropicTvHard==False:
+        tvWeightHard=15.0
 
 
-
-
+    grayData = []
+    labelsData = []
 
     # do minimalistic raw map presmoothing to remove artifacts
     if sPre>0.0001:
-        rawG = vigra.filters.gaussianSmoothing(numpy.ensure(image,dtype=numpy.float32), sigma=sPre)
+        rawG = vigra.filters.gaussianSmoothing(numpy.require(raw ,dtype=numpy.float32), sigma=sPre)
     else :
-        rawG = numpy.ensure(image,dtype=numpy.float32)
+        rawG = numpy.require(image,dtype=numpy.float32)
+
+
 
 
     # get pmap integral
-    pmapIntegral = vigra.filters.gaussianSmoothing(numpy.ensure(pmap,dtype=numpy.float32), sigma=sInt ) )
+    pmapIntegral = vigra.filters.gaussianSmoothing(numpy.require(pmap, dtype=numpy.float32), sigma=sInt )
+    pmapIntegral = numpy.array(pmapIntegral)
     
+    grayData.append([rawG,'rawG'])
+    grayData.append([pmapIntegral,'pmapIntegral'])
+
+    if visu:
+        addHocViewer(grayData, labelsData, visu=visu)
 
     # remap integral 
     pmapIntegral[pmapIntegral>mapInterval]=mapInterval
     pmapIntegral*=1.0/mapInterval
 
 
+
+    print "soft tv"
     # do soft TV smoothing
-    pmapTVSoft = denoise.tvBregman(pmap, weight=tvWeightSoft, isotropicTv=isotropicTvSoft).astype(numpy.float32)
+    pmapTVSoft = denoise.tvBregman(pmap, weight=tvWeightSoft, isotropic=isotropicTvSoft).astype(numpy.float32)
 
 
+    print "hard tv"
     # do hard heavy TV smoothing
-    pmapTVHard = denoise.tvBregman(pmap, weight=tvWeightHard, isotropicTv=isotropicTvHard).astype(numpy.float32)
+    pmapTVHard = denoise.tvBregman(pmap, weight=tvWeightHard, isotropic=isotropicTvHard).astype(numpy.float32)
 
-    
+
+
+    grayData.append([pmapTVSoft,'pmapTVSoft'])
+    grayData.append([pmapTVHard,'pmapTVHard'])
+
+    if visu:
+        addHocViewer(grayData, labelsData, visu=visu)
+
+
     # mix hard and soft according to pmap probability
     mixedPmap = numpy.empty(raw.shape)
     mixedPmap = (1.0 - pmapIntegral)*pmapTVHard  +  pmapIntegral*pmapTVSoft
     
 
+    print "le min le max",mixedPmap.min(), mixedPmap.max()
+
+    #grayData.append([mixedPmap,'mixedPmap'])
+    #addHocViewer(grayData, labelsData, visu=visu)
 
     # add a tiny portion of eigenvalues of hessian give flat wide boundaries the min at the right posstion
     # but we only add this at places where the boundary is strong (in a hard fashion)
-    aew = vigra.filters.hessianOfGaussianEigenvalues(numpy.ensure(image,dtype=numpy.float32), scale=scaleEw).squeeze()
+    aew = vigra.filters.hessianOfGaussianEigenvalues(numpy.require(raw, dtype=numpy.float32), scale=scaleEw).squeeze()
     sew = numpy.sort(aew,axis=3)
     ew = sew[:, :, :, 2]
     ew *= pmap**2 
@@ -139,11 +166,23 @@ def prepareMinMap(raw, pmap, sPre=0.8, sInt = 6.0, mapInterval = 0.3,
     ew *= ewBeta
 
     mixedPmap+=ew
+
+
+    grayData.append([mixedPmap,'mixedPmapWITHEW'])
+    if visu:
+        addHocViewer(grayData, labelsData, visu=visu)
+
+
     # do minimalistic final smoothing to remove artefacts
     if sPre>0.0001:
-        mixedPmapG = vigra.filters.gaussianSmoothing(numpy.ensure(mixedPmap,dtype=numpy.float32), sigma=sPost)
+        mixedPmapG = vigra.filters.gaussianSmoothing(numpy.require(mixedPmap,dtype=numpy.float32), sigma=sPost)
     else :
-        mixedPmapG = numpy.ensure(mixedPmap,dtype=numpy.float32)
+        mixedPmapG = numpy.require(mixedPmap,dtype=numpy.float32)
+
+    grayData.append([mixedPmapG,'finalSeedingMap'])
+    if visu:
+        addHocViewer(grayData, labelsData, visu=visu)
+
 
     return mixedPmapG
 
